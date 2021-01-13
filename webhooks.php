0fa5657e77cc580891c98a75f059067cf2220e34
icekunx <icekunxp@gmail.com>
2021-01-13 10:21:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'NO6JVnhvX6CqofGEHiqE2dA541+6L01Nk2+vpEjGUwSNYa6o52DmZWfDgQj8Ceybn0wDE1oh+m1Pb78jzc3uRkJbhHvjVM4mI9US1uRLUGf+xwmxy7xsqJchK9MxiPgo/wD+282c6W5dbYpxvBmF3QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
